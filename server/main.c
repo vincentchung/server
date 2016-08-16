21345e8c29c711242e75873cf9a41d997aded706
vincent chung <vincentcw.chung@gmail.com>
2016-08-16 17:03:19 +0800

    Initial Commit

---
+++ server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  server
+//
+//  Created by 鐘淳文 on 2016/8/16.
+//  Copyright (c) 2016年 vincent. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
