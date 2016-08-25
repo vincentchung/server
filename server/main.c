384befd22bd5a013d3839b602786b20f869fd463
vincent chung <vincentcw.chung@gmail.com>
2016-08-25 16:58:24 +0800

    update

--- server/main.c
+++ server/main.c
@@ -111,14 +111,27 @@ void insert_queue(char* uid,char* msg,char* suid)
 
 int dequeue(char* uid)
 {
-    for(int i=0;i<rear;i++)
+    for(int i=0;i<=rear;i++)
     {
-        if(!strcmp(unicast_queue[i].TUID,uid))
+        if(!unicast_queue[i].issend)
         {
-            return i;
+            if(!strcmp(unicast_queue[i].TUID,uid))
+            {
+                return i;
+            }
         }
     }
     return -1;
+}
+
+void listqueue()
+{
+    puts("listqueue:");
+    for(int i=0;i<rear;i++)
+    {
+        //sprintf(<#char *restrict#>, <#const char *restrict, ...#>)
+        puts(unicast_queue[i].SUID);
+    }
 }
 //create one more thread for sending muticast message UDP sender!!!
 
@@ -142,7 +155,8 @@ int main(int argc , char *argv[])
     
     
     scanf("%s" , input_msg);
-    
+    //while(1)
+        
     return 0;
 }
 ////////login ID/PW table
@@ -151,15 +165,24 @@ int main(int argc , char *argv[])
 
 int check_login(char* pID,char* pPW)
 {
+    //printf("ID:%s,pwd:%s",pID,pPW);
+    //puts(pID);
+    //puts(pPW);
+    //puts(strlen(pPW));
+    
     for(int i=0;i<ACCOUNT_NUM;i++)
     {
         if(0==strcmp(pID,UIDARRAY[i]))
         {
+            //puts("ID pass");
+            
             if(0==strcmp(pPW,UPWDARRAY[i]))
             {
+                puts("login pass");
                 return 1;
             }else
             {
+                puts("PWD fail");
                 return 0;
             }
         }
@@ -184,70 +207,109 @@ void *server_UDP_handler(void *temp)
     /*Configure settings in address struct*/
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(7891);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
     
     
     /*Bind socket with address struct*/
     bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
     
+    
+    //create a thread to handle
+    
     /*Initialize size variable to be used later on*/
     addr_size = sizeof serverStorage;
     
-    message = "connected ACK\n";
-    sendto(udpSocket,message,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
-    
+    //message = "connected ACK\n";
+    //sendto(udpSocket,message,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+    //UDP msg format
+    //[UID,TCP command format]
     
     while(1){
         /* Try to receive any incoming UDP datagram. Address and port of
          requesting client will be stored on serverStorage variable */
+        memset(client_message, 0, MESSAGE_SIZE);
         nBytes = recvfrom(udpSocket,client_message,MESSAGE_SIZE,0,(struct sockaddr *)&serverStorage, &addr_size);
         
         /*Convert message received to uppercase*/
         /*Send uppercase message back to client, using serverStorage as the address*/
         //sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
-        
+        //char UID[UID_LENGTH];
         puts(client_message);
-        switch(client_message[0])
+        
+        char* UID = strtok(client_message, ",");
+        char* UDPclient_message=client_message+strlen(UID)+1;
+        //puts(UDPclient_message);
+        switch(UDPclient_message[0])
         {
             case 'L':
             {
-                
-                
-                char* loginID = strtok(client_message, ",");
+                char* loginID = strtok(UDPclient_message, ",");
                 char* loginPW=strtok(NULL, ",");
                 
                 if (check_login(loginID+1,loginPW)) {
                     login_state=1;
                     message="login success\n";
-                    sendto(udpSocket,message,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+                    sendto(udpSocket,message,strlen(message),0,(struct sockaddr *)&serverStorage,addr_size);
                 }else
                 {
                     message="login error\n";
-                    sendto(udpSocket,client_message,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
-                    free(udpSocket);
-                    return 0;
+                    sendto(udpSocket,message,strlen(message),0,(struct sockaddr *)&serverStorage,addr_size);
+                    //free(udpSocket);
+                    //return 0;
                 }
                 
             }
                 break;
             case 'A':
-                message="ACK\n";
+            {
+                char temp[6]="ACK";
                 //if(testcounter<5)
-                    sendto(udpSocket,client_message,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
-                
+                sendto(udpSocket,temp,strlen(temp),0,(struct sockaddr *)&serverStorage,addr_size);
                 testcounter++;
-                
+            }
                 break;
             case 'M':
             {
-                message=client_message+1;
+                char temp[6]="ACK";
+                message=UDPclient_message+1;
                 send_UDP_Multicast(message);
+                
+                //if(testcounter<5)
+                sendto(udpSocket,temp,strlen(temp),0,(struct sockaddr *)&serverStorage,addr_size);
             }
                 break;
-            case 'T':
-                //write(sock , client_message , strlen(client_message));
+            case 'C':
+                message = "UDP connected ACK\n";
+                sendto(udpSocket,message,strlen(message),0,(struct sockaddr *)&serverStorage,addr_size);
+                break;
+            case 'P':
+            {
+                char temp[MESSAGE_SIZE];
+                int id=dequeue(UID);
+                if(id!=-1)
+                {
+                    unicast_queue[id].issend=1;
+                    sprintf(temp, "%s:%s",unicast_queue[id].SUID,unicast_queue[id].msg);
+                    sendto(udpSocket,temp,strlen(temp),0,(struct sockaddr *)&serverStorage,addr_size);
+                }else{
+                    char temp[6]="NONE";
+                    //if(testcounter<5)
+                    sendto(udpSocket,temp,strlen(temp),0,(struct sockaddr *)&serverStorage,addr_size);
+                }
+            }
+                break;
+            case 'U':
+            {
+                //send msg to target UID
+                char* sendUID = strtok(UDPclient_message, ",");
+                char* msg=strtok(NULL, ",");
+                insert_queue(sendUID+1, msg, UID);
                 message="ACK\n";
+                sendto(udpSocket,message,strlen(message),0,(struct sockaddr *)&serverStorage,addr_size);
+                listqueue();
+            }
                 break;
         }
     }
@@ -335,15 +397,23 @@ void *connection_handler(void *socket_desc)
     char UID[UID_LENGTH];
     
     //Send some messages to the client
-    message = "connected ACK\n";
-    write(sock , message , strlen(message));
-    
+    while(1)
+    {
+        if( (read_size = recv(sock , client_message , MESSAGE_SIZE , 0)) > 0 )
+        {
+            message = "TCP connected ACK\n";
+            write(sock , message , strlen(message));
+            break;
+        }
+    }
     
     //login ID/PW
     
     //Receive a message from client
     while(1)
     {
+        //listqueue();
+        memset(client_message, 0, MESSAGE_SIZE);
         if( (read_size = recv(sock , client_message , MESSAGE_SIZE , 0)) > 0 )
         {
             //Send the message back to client
@@ -383,6 +453,8 @@ void *connection_handler(void *socket_desc)
                 {
                     message=client_message+1;
                     send_UDP_Multicast(message);
+                    message="ACK\n";
+                    write(sock , message , strlen(message));
                 }
                     break;
                 case 'T':
@@ -394,7 +466,10 @@ void *connection_handler(void *socket_desc)
                     //send msg to target UID
                     char* sendUID = strtok(client_message, ",");
                     char* msg=strtok(NULL, ",");
-                    insert_queue(sendUID, msg, UID);
+                    insert_queue(sendUID+1, msg, UID);
+                    message="ACK";
+                    write(sock , message , strlen(message));
+                    listqueue();
                 }
                     break;
             }
@@ -404,9 +479,12 @@ void *connection_handler(void *socket_desc)
         {
             char temp[MESSAGE_SIZE];
             int id=dequeue(UID);
-            unicast_queue[id].issend=1;
-            sprintf(temp, "%s:%s",unicast_queue[id].SUID,unicast_queue[id].msg);
-            write(sock , unicast_queue[id].msg , strlen(unicast_queue[id].msg));
+            if(id!=-1)
+            {
+                unicast_queue[id].issend=1;
+                sprintf(temp, "%s:%s",unicast_queue[id].SUID,unicast_queue[id].msg);
+                write(sock , temp , strlen(temp));
+            }
         }
     }
     
@@ -435,40 +513,6 @@ void *connection_handler(void *socket_desc)
  M:message   format:M[UID]:[message string]
  B:broadcast format:B[message string]
  */
-
-void handle_message(char* recv_msg,char* resp_msg)
-{
-    switch(recv_msg[0])
-    {
-        case 'L':
-        {
-            
-            char* temp=recv_msg+1;
-            char* loginID = strtok(temp, ",");
-            char* loginPW=strtok(NULL, temp);
-            
-            if (check_login(loginID,loginPW)) {
-                sprintf(resp_msg, "login success");
-            }
-        }
-            break;
-        case 'A':
-            sprintf(resp_msg, "ACK");
-            break;
-            
-        case 'M'://Mulitcast message
-        {
-            char* temp=recv_msg+1;
-            send_UDP_Multicast(temp);
-            //write(sock , client_message , strlen(client_message));
-        }
-            break;
-        case 'U':
-            //write(sock , client_message , strlen(client_message));
-            //message="ACK\n";
-            break;
-    }
-}
 
 //sending multicast message
 
